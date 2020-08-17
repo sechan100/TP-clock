fcaa54930dd81b3784a6762149ea4b7300efa3d5
sechan100 <69431388+sechan100@users.noreply.github.com>
2020-08-17 22:59:20 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,5 +1,5 @@
 const body = document.querySelector('body');
-
+const bgBtn = document.querySelector('#bg_Btn')
 const imgNumber = 4; 
 
 
@@ -9,16 +9,44 @@ function genRandom() {
     return number + 1;
 };
 
-function paintImage(){
+function paintImg(){
     const img = new Image();
-    img.src = `img/${genRandom()}.webp`;
+    const random_N = genRandom();
+    img.src = `img/${random_N}.webp`;
     body.appendChild(img);
+    img.id = `bg${random_N}`;
     img.classList.add("bgimage")
+};
+
+function handle_bg() {
+    bgBtn.addEventListener('click',paint_other_Img);
+};
+
+function paint_other_Img() {
+    const bg = document.querySelector('.bgimage')
+    var i = "none";
+    while(i !== bg.id){
+        const random_N = genRandom();
+        genRandom()
+        body.removeChild(bg);
+        if(random_N !== bg.id){
+            const img = new Image();
+            const random_N = genRandom();
+            img.src = `img/${random_N}.webp`;
+            body.appendChild(img);
+            img.id = `bg${random_N}`;
+            img.classList.add("bgimage")
+            i = `bg${random_N}`
+        }else{
+            i = "none";
+        };
+    };
 };
 
 
 function init() {
-    paintImage();
+    paintImg();
+    handle_bg();
 };
 
 init();
